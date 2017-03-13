07555bdeedb00c5e1040655f7bbc249db5a17fd9
Andrew <andrew@retailreinvented.com>
2017-03-13 13:21:36 -0400

    Update admin menu

---
+++ app/code/local/Xpscommerce/CustomoptionsPricegrid/Block/Adminhtml/Customoptionspricegrid.php
@@ -0,0 +1,11 @@
+<?php
+
+class Xpscommerce_CustomoptionsPricegrid_Block_Adminhtml_Customoptionspricegrid 
+  extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
